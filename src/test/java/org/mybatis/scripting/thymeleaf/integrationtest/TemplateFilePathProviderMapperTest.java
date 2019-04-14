3abeb075547aa5bacf0b40ee87d97502b1fb7d06
Kazuki Shimizu <kazuki43zoo@gmail.com>
2019-04-14 14:39:41 +0900

    Add SQL provider class that help detecting a template file automatically Fixes gh-10

---
+++ src/test/java/org/mybatis/scripting/thymeleaf/integrationtest/TemplateFilePathProviderMapperTest.java
@@ -0,0 +1,127 @@
+/**
+ *    Copyright 2018-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.scripting.thymeleaf.integrationtest;
+
+import java.io.Reader;
+import java.sql.Connection;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.hsqldb.jdbc.JDBCDataSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.mybatis.scripting.thymeleaf.ThymeleafLanguageDriver;
+import org.mybatis.scripting.thymeleaf.integrationtest.domain.Name;
+import org.mybatis.scripting.thymeleaf.integrationtest.mapper.TemplateFilePathProviderMapper;
+import org.mybatis.scripting.thymeleaf.support.TemplateFilePathProvider;
+
+@DisabledIfSystemProperty(named = "mybatis.version", matches = "3\\.4\\..*|3\\.5\\.0")
+class TemplateFilePathProviderMapperTest {
+  private static SqlSessionFactory sqlSessionFactory;
+
+  @BeforeAll
+  static void setUp() throws Exception {
+    Class.forName("org.hsqldb.jdbcDriver");
+    JDBCDataSource dataSource = new JDBCDataSource();
+    dataSource.setUrl("jdbc:hsqldb:mem:db1");
+    dataSource.setUser("sa");
+    dataSource.setPassword("");
+
+    try (Connection conn = dataSource.getConnection()) {
+      try (Reader reader = Resources.getResourceAsReader("create-db.sql")) {
+        ScriptRunner runner = new ScriptRunner(conn);
+        runner.setLogWriter(null);
+        runner.setErrorLogWriter(null);
+        runner.runScript(reader);
+        conn.commit();
+      }
+    }
+
+    TemplateFilePathProvider.setPrefix("sql/");
+    TemplateFilePathProvider.setIncludesPackagePath(false);
+
+    TransactionFactory transactionFactory = new JdbcTransactionFactory();
+    Environment environment = new Environment("development", transactionFactory, dataSource);
+
+    Configuration configuration = new Configuration(environment);
+    configuration.setMapUnderscoreToCamelCase(true);
+    configuration.setDefaultScriptingLanguage(ThymeleafLanguageDriver.class);
+
+    configuration.addMapper(TemplateFilePathProviderMapper.class);
+    sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+  }
+
+  @Test
+  void testInsert() {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      TemplateFilePathProviderMapper mapper = sqlSession.getMapper(TemplateFilePathProviderMapper.class);
+      Name name = new Name();
+      name.setFirstName("Thymeleaf");
+      name.setLastName("MyBatis");
+      mapper.insert(name);
+
+      Name loadedName = mapper.findById(name.getId());
+      Assertions.assertEquals(name.getFirstName(), loadedName.getFirstName());
+      Assertions.assertEquals(name.getLastName(), loadedName.getLastName());
+    }
+  }
+
+  @Test
+  void testUpdate() {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      TemplateFilePathProviderMapper mapper = sqlSession.getMapper(TemplateFilePathProviderMapper.class);
+      Name name = new Name();
+      name.setFirstName("Thymeleaf");
+      name.setLastName("MyBatis");
+      mapper.insert(name);
+
+      Name updatingName = new Name();
+      updatingName.setId(name.getId());
+      updatingName.setFirstName("Thymeleaf3");
+      mapper.update(updatingName);
+
+      Name loadedName = mapper.findById(name.getId());
+      Assertions.assertEquals(updatingName.getFirstName(), loadedName.getFirstName());
+      Assertions.assertEquals(name.getLastName(), loadedName.getLastName());
+    }
+  }
+
+  @Test
+  void testDelete() {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      TemplateFilePathProviderMapper mapper = sqlSession.getMapper(TemplateFilePathProviderMapper.class);
+      Name name = new Name();
+      name.setFirstName("Thymeleaf");
+      name.setLastName("MyBatis");
+      mapper.insert(name);
+
+      mapper.delete(name);
+
+      Name loadedName = mapper.findById(name.getId());
+      Assertions.assertNull(loadedName);
+    }
+  }
+
+}
---
+++ src/test/java/org/mybatis/scripting/thymeleaf/support/TemplateFilePathProviderTest.java
@@ -0,0 +1,149 @@
+/**
+ *    Copyright 2018-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.scripting.thymeleaf.support;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.mybatis.scripting.thymeleaf.ThymeleafLanguageDriverConfig;
+
+@DisabledIfSystemProperty(named = "mybatis.version", matches = "3\\.4\\..*|3\\.5\\.0")
+class TemplateFilePathProviderTest {
+
+  @BeforeEach
+  @AfterEach
+  void clean() {
+    TemplateFilePathProvider.setPrefix(null);
+    TemplateFilePathProvider.setIncludesPackagePath(true);
+    TemplateFilePathProvider.setSeparateDirectoryPerMapper(true);
+    TemplateFilePathProvider.setIncludesMapperNameWhenSeparateDirectory(true);
+    TemplateFilePathProvider.setCustomTemplateFilePathGenerator(null);
+    TemplateFilePathProvider.setLanguageDriverConfig(null);
+  }
+
+  @Test
+  void withoutDatabaseId() {
+    String path = TemplateFilePathProvider.providePath(TestMapper.class, extractMethod(TestMapper.class, "update"),
+        null);
+    Assertions.assertEquals("org/mybatis/scripting/thymeleaf/support/TestMapper/TestMapper-update.sql", path);
+  }
+
+  @Test
+  void withDatabaseId() {
+    String path = TemplateFilePathProvider.providePath(TestMapper.class, extractMethod(TestMapper.class, "update"),
+        "h2");
+    Assertions.assertEquals("org/mybatis/scripting/thymeleaf/support/TestMapper/TestMapper-update-h2.sql", path);
+  }
+
+  @Test
+  void fallbackWithDefaultDatabase() {
+    String path = TemplateFilePathProvider.providePath(TestMapper.class, extractMethod(TestMapper.class, "delete"),
+        "h2");
+    Assertions.assertEquals("org/mybatis/scripting/thymeleaf/support/TestMapper/TestMapper-delete.sql", path);
+  }
+
+  @Test
+  void fallbackDeclaringClassWithoutDatabaseId() {
+    String path = TemplateFilePathProvider.providePath(TestMapper.class, extractMethod(TestMapper.class, "insert"),
+        null);
+    Assertions.assertEquals("org/mybatis/scripting/thymeleaf/support/BaseMapper/BaseMapper-insert.sql", path);
+  }
+
+  @Test
+  void fallbackDeclaringClassWithDatabaseId() {
+    String path = TemplateFilePathProvider.providePath(TestMapper.class, extractMethod(TestMapper.class, "insert"),
+        "h2");
+    Assertions.assertEquals("org/mybatis/scripting/thymeleaf/support/BaseMapper/BaseMapper-insert-h2.sql", path);
+  }
+
+  @Test
+  void fallbackDeclaringClassAndDefaultDatabase() {
+    String path = TemplateFilePathProvider.providePath(TestMapper.class, extractMethod(TestMapper.class, "count"),
+        "h2");
+    Assertions.assertEquals("org/mybatis/scripting/thymeleaf/support/BaseMapper/BaseMapper-count.sql", path);
+  }
+
+  @Test
+  void notFoundSqlFile() {
+    IllegalStateException e = Assertions.assertThrows(IllegalStateException.class, () -> {
+      TemplateFilePathProvider.providePath(TestMapper.class, extractMethod(TestMapper.class, "selectOne"), "h2");
+    });
+    Assertions.assertEquals(
+        "The SQL template file not found. mapperType:[interface org.mybatis.scripting.thymeleaf.support.TestMapper] mapperMethod:[public abstract java.lang.Object org.mybatis.scripting.thymeleaf.support.BaseMapper.selectOne(int)] databaseId:[h2]",
+        e.getMessage());
+  }
+
+  @Test
+  void includesPackagePathAndSeparatesDirectoryPerMapperIsFalse() {
+    TemplateFilePathProvider.setIncludesPackagePath(false);
+    TemplateFilePathProvider.setSeparateDirectoryPerMapper(false);
+    TemplateFilePathProvider.setLanguageDriverConfig(ThymeleafLanguageDriverConfig
+        .newInstance(c -> c.getTemplateFile().setBaseDir("org/mybatis/scripting/thymeleaf/support/sql")));
+    String path = TemplateFilePathProvider.providePath(TestMapper.class,
+        extractMethod(TestMapper.class, "selectAllDesc"), null);
+    Assertions.assertEquals("TestMapper-selectAllDesc.sql", path);
+  }
+
+  @Test
+  void baseDirEndWithSlash() {
+    TemplateFilePathProvider.setIncludesPackagePath(false);
+    TemplateFilePathProvider.setSeparateDirectoryPerMapper(false);
+    TemplateFilePathProvider.setLanguageDriverConfig(ThymeleafLanguageDriverConfig
+        .newInstance(c -> c.getTemplateFile().setBaseDir("org/mybatis/scripting/thymeleaf/support/sql/")));
+    String path = TemplateFilePathProvider.providePath(TestMapper.class,
+        extractMethod(TestMapper.class, "selectAllDesc"), null);
+    Assertions.assertEquals("TestMapper-selectAllDesc.sql", path);
+  }
+
+  @Test
+  void includesMapperNameWhenSeparateDirectoryIsFalse() {
+    TemplateFilePathProvider.setIncludesMapperNameWhenSeparateDirectory(false);
+    String path = TemplateFilePathProvider.providePath(TestMapper.class,
+        extractMethod(TestMapper.class, "selectAllAsc"), null);
+    Assertions.assertEquals("org/mybatis/scripting/thymeleaf/support/TestMapper/selectAllAsc.sql", path);
+  }
+
+  @Test
+  void prefix() {
+    TemplateFilePathProvider.setPrefix("org/mybatis/scripting/thymeleaf/support/sql/");
+    TemplateFilePathProvider.setIncludesPackagePath(false);
+    TemplateFilePathProvider.setSeparateDirectoryPerMapper(false);
+    String path = TemplateFilePathProvider.providePath(TestMapper.class,
+        extractMethod(TestMapper.class, "selectAllDesc"), null);
+    Assertions.assertEquals("org/mybatis/scripting/thymeleaf/support/sql/TestMapper-selectAllDesc.sql", path);
+  }
+
+  @Test
+  void customTemplateFileGenerator() {
+    TemplateFilePathProvider.setCustomTemplateFilePathGenerator(
+        (type, method, databaseId) -> type.getName().replace('.', '/') + "_" + method.getName() + ".sql");
+    String path = TemplateFilePathProvider.providePath(TestMapper.class, extractMethod(TestMapper.class, "selectOne"),
+        null);
+    Assertions.assertEquals("org/mybatis/scripting/thymeleaf/support/BaseMapper_selectOne.sql", path);
+
+  }
+
+  private Method extractMethod(Class<?> type, String methodName) {
+    return Arrays.stream(type.getMethods()).filter(m -> m.getName().equals(methodName)).findFirst().orElseThrow(
+        () -> new IllegalArgumentException("The method not found. type:" + type + " methodName:" + methodName));
+  }
+
+}
